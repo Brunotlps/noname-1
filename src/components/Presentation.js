d73f94a8488d50878a48113a2526e74927ad0e79
bruno <brunoteixlps@gmail.com>
2025-07-15 19:40:42 -0300

    8° commit - Alguns ajustes em Presentation e projects

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -5,7 +5,7 @@ export default function Presentation() {
     return (
         <section className="presentation">
             <p>
-                Atuo como Desenvolvedor Fullstack, criando projetos responsivos, escaláveis e pensados para crescer junto com quem usa. Gosto de transformar sonhos (ou ideias malucas) em soluções reais, mantendo sempre o equilíbrio entre o lado criativo e técnico do desenvolvimento.
+                Atuo como Desenvolvedor Full stack, criando projetos responsivos, escaláveis e pensados para crescer junto com quem usa. Gosto de transformar sonhos (ou ideias malucas) em soluções reais, mantendo sempre o equilíbrio entre o lado criativo e técnico do desenvolvimento.
                 A cada novo projeto, meu objetivo é entregar não só funcionalidade, mas também propósito, experiência e claro, aprender e evoluir um pouco mais.
             </p>
         </section>
